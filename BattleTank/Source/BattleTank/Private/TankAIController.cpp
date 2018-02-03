e7adef61401af513ff13ccbb64041def221e3cff
BlueFibre <bluefibregames@gmail.com>
2018-02-03 03:14:30 +0000

    - TankAIController can now detect player pawn

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,25 +1,32 @@
 // Copyright David Tootell 2018
 
 #include "TankAIController.h"
+#include "Runtime/Engine/Classes/Engine/World.h"
 
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	auto ControlledTank = GetControlledTank();
-	if (!ControlledTank)
+	auto PlayerTank = GetControlledTank();
+	if (!PlayerTank)
 	{
-		UE_LOG( LogTemp, Warning, TEXT( "TankAIController not possesing a tank" ) );
+		UE_LOG( LogTemp, Warning, TEXT( "TankAIController cant find player tank" ) );
 	}
 	else
 	{
-		UE_LOG( LogTemp, Warning, TEXT( "TankAIController: %s" ), *(ControlledTank->GetName()) );
+		UE_LOG( LogTemp, Warning, TEXT( "AIController found player tank: %s" ), *(PlayerTank->GetName()) );
 	}
 }
 
 ATank* ATankAIController::GetControlledTank() const
 {
 	return Cast<ATank>( GetPawn() );
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	return Cast<ATank>( PlayerPawn );
 }
 
 
